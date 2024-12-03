75040139becd3a01d4c08feab56038faaef9da0a
Pavithra-0411 <cherukupallypavithrayadav2019@gmail.com>
2024-12-03 21:31:28 +0530

    index.js added

---
+++ index.js
@@ -0,0 +1,18 @@
+arr=[1,'cat',2.35,true]
+for(i=0;i<arr.length;i++)
+{
+    console.log(arr[i]);
+}
+
+//length
+console.log("Length of arr -> "+arr.length);
+
+//pop
+arr.pop()
+
+console.log("after pop => "+arr);
+
+//push
+
+arr.push("Anudeep Durishetty")
+console.log("after push => "+arr);
